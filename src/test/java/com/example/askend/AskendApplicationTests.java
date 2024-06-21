143662f05d40645f4007b5bcb1bd836f5c3096ff
tiit.kroon <tiit.kroon@tietoevry.com>
2024-06-21 08:33:48 +0300

    Adding Spring web and H2

---
+++ src/test/java/com/example/askend/AskendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.askend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
